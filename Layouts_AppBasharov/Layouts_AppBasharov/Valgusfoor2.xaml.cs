27925e0435bc0b22feaac1063da7b4117ccbcb50
opilane <opilane@HP-CZC6297B8C>
2020-09-03 10:44:22 +0300

    Добавление кнопок и Фреймов

--- Layouts_AppBasharov/Layouts_AppBasharov/Valgusfoor2.xaml.cs
+++ Layouts_AppBasharov/Layouts_AppBasharov/Valgusfoor2.xaml.cs
@@ -22,12 +22,26 @@ namespace Layouts_AppBasharov
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
+            Frame pun = new Frame()
+            {
+                BackgroundColor = Color.Red,
+                Content = punane,
+                CornerRadius = 80,
+                Margin = new Thickness(40, 115, 40, 115)
+            };
             Label kollane = new Label()
             {
                 Text = "Kollane",
                 TextColor = Color.Yellow,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
+            };
+            Frame kol = new Frame()
+            {
+                BackgroundColor = Color.Yellow,
+                Content = kollane,
+                CornerRadius = 80,
+                Margin = new Thickness(40, 115, 40, 115)
             };
             Label roheline = new Label()
             {
@@ -36,14 +50,27 @@ namespace Layouts_AppBasharov
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
-            Frame frame = new Frame()
+            Frame roh = new Frame()
             {
+                BackgroundColor = Color.Green,
+                Content = roheline,
+                CornerRadius = 80,
+                Margin = new Thickness(80, 0, 80, 0),
+                VerticalOptions = 
+            };
+            Button but = new Button()
+            {
+                Text = "Välja",
+                BackgroundColor = Color.Gray,
 
             };
-
+            Button but2 = new Button()
+            {
+                Text = "Sisse",
+            };
             StackLayout stackLayout = new StackLayout()
             {
-                Children = { punane, kollane, roheline }
+                Children = { pun, kol, roh, but, but2 }
             };
             stackLayout.Orientation = StackOrientation.Horizontal;
             Content = stackLayout;
